e65ceace7009b8a6518f0fa03af8a10d8b65912f
Akilesh <akileshakileshs1234@gmail.com>
2025-06-02 00:41:25 +0530

    Added a controller in Categories to get the products by the category.

--- routes/categories.js
+++ routes/categories.js
@@ -6,10 +6,15 @@ import {
   getCategoryById,
   updateCategory,
   deleteCategory,
+  getProductsByCategory,
 } from "../controllers/categoryController.js";
 
+// Specific routes first
 router.post("/", createCategory);
 router.get("/", getAllCategories);
+router.post("/products", getProductsByCategory);
+
+// Parameterized routes last
 router.get("/:id", getCategoryById);
 router.put("/:id", updateCategory);
 router.delete("/:id", deleteCategory);
